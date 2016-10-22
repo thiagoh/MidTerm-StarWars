7da63a125f63deac7c85f6d803e69fe7667e69b0
Thiago Andrade <thiagoh@gmail.com>
2016-10-22 13:24:05 -0400

    working prototype

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,55 +2,55 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	// PUBLIC INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++
-	//public float speed;
-	public Boundary boundary;
+    // PUBLIC INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++
+    //public float speed;
+    public Boundary boundary;
+    public float speed;
+    public Camera camera;
 
-	public Camera camera;
-	
-	// PRIVATE INSTANCE VARIABLES
-	private Vector2 _newPosition = new Vector2(0.0f, 0.0f);
-	
-	// Use this for initialization
-	void Start () {
+    // PRIVATE INSTANCE VARIABLES
+    private Vector2 _newPosition = new Vector2(0.0f, 0.0f);
 
-	}
+    // Use this for initialization
+    void Start() {
 
-	// Update is called once per frame
-	void Update () {
-		this._CheckInput ();
-	}
+    }
 
-	private void _CheckInput() {
-		this._newPosition = gameObject.GetComponent<Transform> ().position; // current position
+    // Update is called once per frame
+    void Update() {
+        this._CheckInput();
+    }
 
-		/* movement by keyboard
-		if (Input.GetAxis ("Horizontal") > 0) {
-			this._newPosition.x += this.speed; // add move value to current position
-		}
-	
-		
-		if (Input.GetAxis ("Horizontal") < 0) {
-			this._newPosition.x -= this.speed; // subtract move value to current position
-		}
-		*/
+    private void _CheckInput() {
+        this._newPosition = gameObject.GetComponent<Transform>().position; // current position
 
+        if (Input.GetAxis("Horizontal") > 0) {
+            this._newPosition.x += this.speed; // add move value to current position
+        }
+
+
+        if (Input.GetAxis("Horizontal") < 0) {
+            this._newPosition.x -= this.speed; // subtract move value to current position
+        }
+
+        /* movement by keyboard
 		// movement by mouse
 		Vector2 mousePosition = Input.mousePosition;
 		this._newPosition.x = this.camera.ScreenToWorldPoint (mousePosition).x;
+		*/
 
-		this._BoundaryCheck ();
+        this._BoundaryCheck();
 
-		gameObject.GetComponent<Transform>().position = this._newPosition;
-	}
+        gameObject.GetComponent<Transform>().position = this._newPosition;
+    }
 
-	private void _BoundaryCheck() {
-		if (this._newPosition.x < this.boundary.xMin) {
-			this._newPosition.x = this.boundary.xMin;
-		}
+    private void _BoundaryCheck() {
+        if (this._newPosition.x < this.boundary.xMin) {
+            this._newPosition.x = this.boundary.xMin;
+        }
 
-		if (this._newPosition.x > this.boundary.xMax) {
-			this._newPosition.x = this.boundary.xMax;
-		}
-	}
+        if (this._newPosition.x > this.boundary.xMax) {
+            this._newPosition.x = this.boundary.xMax;
+        }
+    }
 }
